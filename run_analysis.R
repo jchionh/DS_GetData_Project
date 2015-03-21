0dab9dcbaabebaa5af4e00fbd69ea306f34331e3
Jason Chionh <jasonchionh@gmail.com>
2015-03-22 05:43:35 +0800

    added comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,13 @@
+#########################################################################################
+# ASSUMPTION:
+# 
+# That the dataset is already downloaded, and unzipped into a sub directory called
+# "UCI HAR Dataset" in the worling directory.
+#
+# the zipped dataset can be obtained from:
+# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+#########################################################################################
+
 ## 1. Merges the training and the test sets to create one data set.
 
 # let's read the training set
