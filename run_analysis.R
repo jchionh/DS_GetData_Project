535187c6de4843a71346bd53b5e8aba4131e3850
Jason Chionh <jasonchionh@gmail.com>
2015-03-22 03:52:22 +0800

    first commit, with part 1 implemented

---
+++ run_analysis.R
@@ -0,0 +1,26 @@
+## 1. Merges the training and the test sets to create one data set.
+
+# let's read the training set
+train_x <- read.table("UCI HAR Dataset/train/X_train.txt", stringsAsFactors=FALSE)
+train_y <- read.table("UCI HAR Dataset/train/y_train.txt", stringsAsFactors=FALSE)
+train_subject <- read.table("UCI HAR Dataset/train/subject_train.txt", stringsAsFactors=FALSE)
+# now, merge to get the training set
+train_data <- cbind(train_y, train_subject, train_x)
+
+# let's read the test set
+test_x <- read.table("UCI HAR Dataset/test/X_test.txt", stringsAsFactors=FALSE)
+test_y <- read.table("UCI HAR Dataset/test/y_test.txt", stringsAsFactors=FALSE)
+test_subject <- read.table("UCI HAR Dataset/test/subject_test.txt", stringsAsFactors=FALSE)
+# now, merge to get the test data set
+test_data <- cbind(test_y, test_subject, test_x)
+
+# now, merge both the test and train data to get the fully merged data
+merged_data <- rbind(train_data, test_data)
+
+# assign column names to the merged data
+features <- read.table("UCI HAR Dataset/features.txt", stringsAsFactors = FALSE)
+names(merged_data) <- c("activity", "subject", features$V2)
+
+
+
+
